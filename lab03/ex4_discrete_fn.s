5f03d782666e9cde652a149d8315467457a0e9b9
dsy <deng.siyang@outlook.com>
2024-02-25 22:22:55 +0800

    (lab3) finished

--- lab03/ex4_discrete_fn.s
+++ lab03/ex4_discrete_fn.s
@@ -6,6 +6,10 @@
 # The return value should be stored in a0
 f:
     # Your code here
+    addi t0, a0, 3 # t0 = a0 + 3, a0 is the value we want to evaluate f at, t0 is the index of the array
+    slli t0, t0, 2 # t0 = t0 * 4, t0 is the offset of the element in the array
+    add t0, t0, a1 # t0 = t0 + a1, t0 is the address of the element in the array
+    lw a0, 0(t0) # a0 = *t0, a0 is the value of the element in the array
 
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
--- lab03/ex5_factorial.s
+++ lab03/ex5_factorial.s
@@ -27,6 +27,17 @@ main:
 # The return value should be stored in a0
 factorial:
     # YOUR CODE HERE
+    add t0, a0, x0 # t0 = a0, t0 is the counter
+    addi t1, x0, 1 # t1 = 1, t1 is the result
+    addi t2, x0, 1 # t2 = 1, t2 is the multiplier
+    loop:
+        beq t0, x0, end # if t0 == 0, jump to end
+        mul t1, t1, t2 # t1 = t1 * t2
+        addi t2, t2, 1 # t2 = t2 + 1
+        addi t0, t0, -1 # t0 = t0 - 1
+        j loop # jump to loop
+    end:
+        add a0, t1, x0 # a0 = t1
 
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
